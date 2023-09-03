f7282839972e67260088f15cdc5a2002910870a5
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-09-03 14:06:30 +0600

    Achivments section

--- src/Component/DeveloperPortfolio/DeveloperAchivments.jsx
+++ src/Component/DeveloperPortfolio/DeveloperAchivments.jsx
@@ -1,11 +1,60 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
 
 class DeveloperAchivments extends Component {
-  render() {
-    return (
-      <div>DeveloperAchivments</div>
-    )
-  }
+    render() {
+      return (
+        <Fragment>
+          <Container className='IntroStart' fluid={true}>
+            <h1 className='TitleTopic'>Achivments</h1>
+            <Row>
+              <Col lg={12} md={12} sm={12}> 
+                <h4 className='awardTitle'>Award Title 1</h4>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer diam orci, mollis a mi in, ullamcorper pretium nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque commodo turpis et lacus accumsan, vel hendrerit odio sodales. Duis tortor mauris, suscipit sit amet mauris ut, pellentesque pulvinar dolor. Morbi eu magna vitae ipsum bibendum ornare ac id elit. Vivamus id volutpat metus, varius fringilla risus. Nulla ullamcorper, lorem quis viverra tincidunt, nulla dui faucibus leo, et vulputate libero dui vel ante.
+                  
+                  Phasellus euismod, nunc eget molestie dapibus, augue turpis condimentum magna, commodo eleifend orci enim eu erat. Praesent quis auctor leo. Donec congue, eros sed vehicula commodo, est mi hendrerit massa, non sodales nisl magna vel tellus. Curabitur quis pellentesque nulla, eu mattis nisi. Morbi lobortis placerat augue nec egestas. Nulla ultrices, augue quis tempor convallis, turpis lorem volutpat nisl, ut mollis augue justo at ex. Morbi sed lacus vitae libero volutpat pretium. In in dui diam. Duis elit erat, venenatis id augue non, euismod tincidunt magna.
+                  
+                  Sed vel quam nec lacus posuere rutrum ultricies ac risus. Proin quis lectus quis ipsum vulputate ornare. Nam ut tristique risus. Proin vitae nisl libero. Nulla sit amet orci eu augue euismod lobortis. Etiam ex lorem, aliquet sed velit quis, lacinia luctus magna. Vivamus fermentum tortor mi, non vestibulum arcu pulvinar ac. Sed et vulputate libero, eu dapibus ipsum. Sed tempor, dolor congue maximus posuere, lectus lacus ornare justo, nec commodo orci dui quis dolor. In hac habitasse platea dictumst. Aliquam erat volutpat.
+                  
+                  Suspendisse porta augue viverra tellus pellentesque maximus. Nulla vitae scelerisque quam. Aliquam sed consectetur nulla. Nam quis dapibus ligula. Cras malesuada sed tellus vel euismod. Nam sed rhoncus lacus, in facilisis dolor. Nulla sem elit, efficitur quis tincidunt id, pretium non turpis. Sed quis sapien ornare augue scelerisque dictum nec eu nulla. Cras fermentum finibus quam a eleifend. Quisque ac faucibus tellus, id sodales mauris. Phasellus sit amet est justo.
+                  
+                  Integer iaculis neque ac ante venenatis, vitae elementum lorem porttitor. Nunc sit amet ligula sed augue sagittis semper a non velit. Duis feugiat consequat nulla, vitae blandit est vehicula eu. Integer volutpat lorem porta arcu bibendum egestas. Phasellus vel tristique est. Cras id porta est, ut bibendum enim. Quisque rutrum massa at nisi pretium, nec tincidunt quam posuere. Fusce pellentesque blandit turpis et egestas. Morbi eu aliquet nulla.
+                </p>
+              </Col>
+              <Col lg={12} md={12} sm={12}> 
+                <h4 className='awardTitle'>Award Title 2</h4>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer diam orci, mollis a mi in, ullamcorper pretium nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque commodo turpis et lacus accumsan, vel hendrerit odio sodales. Duis tortor mauris, suscipit sit amet mauris ut, pellentesque pulvinar dolor. Morbi eu magna vitae ipsum bibendum ornare ac id elit. Vivamus id volutpat metus, varius fringilla risus. Nulla ullamcorper, lorem quis viverra tincidunt, nulla dui faucibus leo, et vulputate libero dui vel ante.
+                  
+                  Phasellus euismod, nunc eget molestie dapibus, augue turpis condimentum magna, commodo eleifend orci enim eu erat. Praesent quis auctor leo. Donec congue, eros sed vehicula commodo, est mi hendrerit massa, non sodales nisl magna vel tellus. Curabitur quis pellentesque nulla, eu mattis nisi. Morbi lobortis placerat augue nec egestas. Nulla ultrices, augue quis tempor convallis, turpis lorem volutpat nisl, ut mollis augue justo at ex. Morbi sed lacus vitae libero volutpat pretium. In in dui diam. Duis elit erat, venenatis id augue non, euismod tincidunt magna.
+                  
+                  Sed vel quam nec lacus posuere rutrum ultricies ac risus. Proin quis lectus quis ipsum vulputate ornare. Nam ut tristique risus. Proin vitae nisl libero. Nulla sit amet orci eu augue euismod lobortis. Etiam ex lorem, aliquet sed velit quis, lacinia luctus magna. Vivamus fermentum tortor mi, non vestibulum arcu pulvinar ac. Sed et vulputate libero, eu dapibus ipsum. Sed tempor, dolor congue maximus posuere, lectus lacus ornare justo, nec commodo orci dui quis dolor. In hac habitasse platea dictumst. Aliquam erat volutpat.
+                  
+                  Suspendisse porta augue viverra tellus pellentesque maximus. Nulla vitae scelerisque quam. Aliquam sed consectetur nulla. Nam quis dapibus ligula. Cras malesuada sed tellus vel euismod. Nam sed rhoncus lacus, in facilisis dolor. Nulla sem elit, efficitur quis tincidunt id, pretium non turpis. Sed quis sapien ornare augue scelerisque dictum nec eu nulla. Cras fermentum finibus quam a eleifend. Quisque ac faucibus tellus, id sodales mauris. Phasellus sit amet est justo.
+                  
+                  Integer iaculis neque ac ante venenatis, vitae elementum lorem porttitor. Nunc sit amet ligula sed augue sagittis semper a non velit. Duis feugiat consequat nulla, vitae blandit est vehicula eu. Integer volutpat lorem porta arcu bibendum egestas. Phasellus vel tristique est. Cras id porta est, ut bibendum enim. Quisque rutrum massa at nisi pretium, nec tincidunt quam posuere. Fusce pellentesque blandit turpis et egestas. Morbi eu aliquet nulla.
+                </p>
+              </Col>
+              <Col lg={12} md={12} sm={12}> 
+                <h4 className='awardTitle'>Award Title 3</h4>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer diam orci, mollis a mi in, ullamcorper pretium nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque commodo turpis et lacus accumsan, vel hendrerit odio sodales. Duis tortor mauris, suscipit sit amet mauris ut, pellentesque pulvinar dolor. Morbi eu magna vitae ipsum bibendum ornare ac id elit. Vivamus id volutpat metus, varius fringilla risus. Nulla ullamcorper, lorem quis viverra tincidunt, nulla dui faucibus leo, et vulputate libero dui vel ante.
+                  
+                  Phasellus euismod, nunc eget molestie dapibus, augue turpis condimentum magna, commodo eleifend orci enim eu erat. Praesent quis auctor leo. Donec congue, eros sed vehicula commodo, est mi hendrerit massa, non sodales nisl magna vel tellus. Curabitur quis pellentesque nulla, eu mattis nisi. Morbi lobortis placerat augue nec egestas. Nulla ultrices, augue quis tempor convallis, turpis lorem volutpat nisl, ut mollis augue justo at ex. Morbi sed lacus vitae libero volutpat pretium. In in dui diam. Duis elit erat, venenatis id augue non, euismod tincidunt magna.
+                  
+                  Sed vel quam nec lacus posuere rutrum ultricies ac risus. Proin quis lectus quis ipsum vulputate ornare. Nam ut tristique risus. Proin vitae nisl libero. Nulla sit amet orci eu augue euismod lobortis. Etiam ex lorem, aliquet sed velit quis, lacinia luctus magna. Vivamus fermentum tortor mi, non vestibulum arcu pulvinar ac. Sed et vulputate libero, eu dapibus ipsum. Sed tempor, dolor congue maximus posuere, lectus lacus ornare justo, nec commodo orci dui quis dolor. In hac habitasse platea dictumst. Aliquam erat volutpat.
+                  
+                  Suspendisse porta augue viverra tellus pellentesque maximus. Nulla vitae scelerisque quam. Aliquam sed consectetur nulla. Nam quis dapibus ligula. Cras malesuada sed tellus vel euismod. Nam sed rhoncus lacus, in facilisis dolor. Nulla sem elit, efficitur quis tincidunt id, pretium non turpis. Sed quis sapien ornare augue scelerisque dictum nec eu nulla. Cras fermentum finibus quam a eleifend. Quisque ac faucibus tellus, id sodales mauris. Phasellus sit amet est justo.
+                  
+                  Integer iaculis neque ac ante venenatis, vitae elementum lorem porttitor. Nunc sit amet ligula sed augue sagittis semper a non velit. Duis feugiat consequat nulla, vitae blandit est vehicula eu. Integer volutpat lorem porta arcu bibendum egestas. Phasellus vel tristique est. Cras id porta est, ut bibendum enim. Quisque rutrum massa at nisi pretium, nec tincidunt quam posuere. Fusce pellentesque blandit turpis et egestas. Morbi eu aliquet nulla.
+                </p>
+              </Col>
+            </Row>
+          </Container>
+        </Fragment>
+      )
+    }
 }
 
 export default DeveloperAchivments
--- src/Component/DeveloperPortfolio/DevelopersIntroduction.jsx
+++ src/Component/DeveloperPortfolio/DevelopersIntroduction.jsx
@@ -1,165 +1,18 @@
 import React, { Component, Fragment } from 'react'
-import { Col, Container, Form, Row, Table } from 'react-bootstrap'
+import { Container, Row } from 'react-bootstrap'
 
 export class DevelopersIntroduction extends Component {
     render() {
       return (
         <Fragment>
           <Container className='IntroStart' fluid={true}>
+            <h1 className='TitleTopic'>Introduction</h1>
             <Row>
-              <Table striped bordered hover>
-                  <thead>
-                    <tr>
-                      <th></th>
-                      <th></th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>Full Name:</td>
-                      <td>Md. Shaidur Rahman</td>
-                    </tr>
-                    <tr>
-                      <td>Education:</td>
-                      <td>Bsc in Software Engineering</td>
-                    </tr>
-                    <tr>
-                      <td>Graduate Status:</td>
-                      <td>Completed (2022)</td>
-                    </tr>
-                    <tr>
-                      <td>University:</td>
-                      <td>Daffodil International University</td>
-                    </tr>
-                  </tbody>
-                </Table>
-                <h4> </h4>
-                <h4>Completed Course: </h4>
-                <Table striped bordered hover>
-                  <thead>
-                    <tr>
-                      <th>Serial</th>
-                      <th>1st Year</th>
-                      <th>2nd Year</th>
-                      <th>3rd Year</th>
-                      <th>4th Year</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>1</td>
-                      <td>Introduction to Software Engineering</td>
-                      <td>Digital Electronics & Logic Design</td>
-                      <td>Design Pattern</td>
-                      <td>Introduction to Machine Learning</td>
-                    </tr>
-                    <tr>
-                      <td>2</td>
-                      <td>English I</td>
-                      <td>Algorithms Design & Analysis</td>
-                      <td>Software Quality Assurance & Testing Lab</td>
-                      <td>Business Analysis & Communication</td>
-                    </tr>
-                    <tr>
-                      <td>3</td>
-                      <td>Art of Living</td>
-                      <td>Algorithms Design & Analysis Lab</td>
-                      <td>Software Quality Assurance & Testing</td>
-                      <td>Software Project Management & Documentation</td>
-                    </tr>
-                    <tr>
-                      <td>4</td>
-                      <td>Computer Fundamentals</td>
-                      <td>Software Requirement Specifications & Analysis</td>
-                      <td>Data Communication & Computer Networking Lab</td>
-                      <td>Compiler Design</td>
-                    </tr>
-                    <tr>
-                      <td>5</td>
-                      <td>Computer Fundamentals Lab</td>
-                      <td>Object Oriented Concepts</td>
-                      <td>Data Communication & Computer Networking</td>
-                      <td>Data Warehouse & Data Mining</td>
-                    </tr>
-                    <tr>
-                      <td>6</td>
-                      <td>Discrete Mathematics</td>
-                      <td>Database System</td>
-                      <td>Employability Skills 360Â°</td>
-                      <td>Bangladesh Studies</td>
-                    </tr>
-                    <tr>
-                      <td>7</td>
-                      <td>Physics I</td>
-                      <td>Database System Lab</td>
-                      <td>Management Information System</td>
-                      <td>Human Computer Interaction</td>
-                    </tr>
-                    <tr>
-                      <td>8</td>
-                      <td>Structured Programming</td>
-                      <td>Computer Architecture</td>
-                      <td>Software Architecture & Design</td>
-                      <td>Final Year Project/ Thesis/ Internship</td>
-                    </tr>
-                    <tr>
-                      <td>9</td>
-                      <td>Structured Programming Lab</td>
-                      <td>Object Oriented Design</td>
-                      <td>Software Engineering Web Application Lab</td>
-                      <td></td>
-                    </tr>
-                    <tr>
-                      <td>10</td>
-                      <td>Mathematics I</td>
-                      <td>Introduction to Robotics</td>
-                      <td>Software Engineering Web Application</td>
-                      <td></td>
-                    </tr>
-                    <tr>
-                      <td>11</td>
-                      <td>Software Development Capstone Project</td>
-                      <td>Theory of Computing</td>
-                      <td>Numerical Analysis</td>
-                      <td></td>
-                    </tr>
-                    <tr>
-                      <td>12</td>
-                      <td>Data Structure Lab</td>
-                      <td>Operating System & System Programming</td>
-                      <td>Software Engineering Professional Ethics</td>
-                      <td></td>
-                    </tr>
-                    <tr>
-                      <td>13</td>
-                      <td>Data Structure</td>
-                      <td>Operating System & System Programming Lab</td>
-                      <td>Information System Security</td>
-                      <td></td>
-                    </tr>
-                    <tr>
-                      <td>14</td>
-                      <td>Mathematics II</td>
-                      <td>Principles of Accounting, Business & Economics</td>
-                      <td>Software Engineering Design Capstone Project</td>
-                      <td></td>
-                    </tr>
-                    <tr>
-                      <td>15</td>
-                      <td>Statistics I</td>
-                      <td>System Analysis & Design Capstone Project</td>
-                      <td>Artificial Intelligence</td>
-                      <td></td>
-                    </tr>
-                    <tr>
-                      <td>16</td>
-                      <td></td>
-                      <td></td>
-                      <td>Artificial Intelligence Lab</td>
-                      <td></td>
-                    </tr>
-                  </tbody>
-                </Table>
+              <img className='DevImg' src='https://img.freepik.com/free-photo/waist-up-portrait-handsome-serious-unshaven-male-keeps-hands-together-dressed-dark-blue-shirt-has-talk-with-interlocutor-stands-against-white-wall-self-confident-man-freelancer_273609-16320.jpg?w=996&t=st=1693726450~exp=1693727050~hmac=2a44d7d862e6ff2fb35602a96fc2475eca531d45a1db770a9600e4cfd042b403' />
+              <h4 className='PortDTitle'>Md. Shaidur Rahman</h4>
+              <h4 className='PortDSub'>B.Sc in Software Engineering , 2022</h4>
+              <h4 className='PortDSub'>Daffodil International University</h4>
+              <h4 className='PortDSub'>Dhaka, Bangladesh</h4>
             </Row>
           </Container>
         </Fragment>
